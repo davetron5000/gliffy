9eeb6b58fbae6cee7c26b12b314a4f23caf4e398
David Copeland <davetron5000@gmail.com>
2009-04-16 08:54:13 -0400

    OAuth style signing implemented

--- lib/gliffy/url.rb
+++ lib/gliffy/url.rb
@@ -1,61 +1,99 @@
 require 'gliffy/config'
+require 'rubygems'
+require 'hmac'
+require 'hmac-sha1'
+require 'base64'
 
 module Gliffy
   # Handles signing and assembling the URL
   class SignedURL
 
-    def initialize(api_key,secret_key,url_root,url)
-      raise ArgumentError.new("api_key is required") if !api_key
-      raise ArgumentError.new("secret_key is required") if !secret_key
-      raise ArgumentError.new("url_root is required") if !url_root
-      raise ArgumentError.new("url is required") if !url_root
+    READ_ONLY_PARAMS = {
+      'oauth_consumer_key' => true,
+      'oauth_token' => true,
+      'oauth_signature_method' => true,
+      'oauth_version' => true,
+      'oauth_nonce' => true,
+      'oauth_timestamp' => true,
+    }
+
+    # Create a new SignedURL with the following options (all required unless otherwise stated):
+    #
+    # [:consumer_key] The OAuth consumer key
+    # [:consumer_secret] The OAuth consumer secret
+    # [:access_token] The OAuth user access token (optional)
+    # [:access_secret] The OAuth user access token (required if :access_token provided, otherwise omit)
+    # [:method] The HTTP Request method that will be made
+    # [:url] The URL (without parameters) to request
+    #
+    def initialize(options)
+      raise ArgumentError.new("consumer_key is required") if !options[:consumer_key]
+      raise ArgumentError.new("consumer_secret is required") if !options[:consumer_secret]
+      raise ArgumentError.new("url is required") if !options[:url]
+      raise ArgumentError.new("method is required") if !options[:method]
       @logger = Logger.new(Config.config.log_device)
       @logger.level = Config.config.log_level
       @params = Hash.new
-      @params['apiKey'] = api_key
-      @secret_key = secret_key
-      @url_root = url_root
-      @url = url
+      @params['oauth_consumer_key'] = options[:consumer_key]
+      @params['oauth_token'] = options[:access_token]
+      @params['oauth_signature_method'] = 'HMAC-SHA1'
+      @params['oauth_version'] = '1.0'
+      @signing_key = "#{options[:consumer_secret]}&#{options[:access_secret]}"
+      @method = options[:method]
+      @url = options[:url]
     end
 
     # Sets a request parameter
     #
-    # [param] the name of the parameter, as a string
+    # [param] the name of the parameter, as a string or symbol
     # [value] the value of the parameter, unencoded
     #
     def []=(param,value)
-      if (param == 'apiKey')
-        raise ArgumentError.new('You may not override the api_key in this way')
-      end
-      @params[param] = value
+      raise ArgumentError.new("param may not be nil") if param.nil?
+      raise ArgumentError.new("value may not be nil") if value.nil?
+      param = param.to_s
+      raise ArgumentError.new("You may not override #{param}") if READ_ONLY_PARAMS[param]
+      @params[param] = value.to_s
     end
 
     # Sets all request parameters to those in the hash.
     def params=(params_hash)
-      api_key = @params['apiKey']
-      @params.replace(params_hash)
-      @params['apiKey'] = api_key
+      params_hash.each do |k,v|
+        self[k]=v
+      end
     end
 
     # Gets the full URL, signed and ready to be requested
-    def full_url
+    def full_url(timestamp=Time.now.to_i,nonce=Time.now.to_i.to_s)
       @logger.debug("Getting full_url of #{@url}")
-      to_sign = @url
+      to_sign = @method + "&" + CGI::escape(@url) + "&"
       url_params = Hash.new
+      param_part = ""
       @params.keys.sort.each do |key|
         @logger.debug("Adding param #{key} (#{to_sign.class.to_s}, #{key.class.to_s}) : #{to_sign.to_s}")
-        to_sign += key
-        to_sign += @params[key].to_s
-        url_params[key.to_s] = @params[key].to_s
+        param_part += key
+        param_part += "="
+        param_part += @params[key]
+        url_params[key] = CGI::escape(@params[key])
       end
-      to_sign += @secret_key
-      @logger.debug("Signing '#{to_sign}'")
-      signature = Digest::MD5.hexdigest(to_sign)
-      signature.gsub!(/^0/,'') while (signature =~ /^0/)
-      @logger.debug("signature == '#{signature}'")
-      url_params['signature'] = signature
+      url_params['oauth_timestamp'] = timestamp.to_s
+      url_params['oauth_nonce'] = nonce
+      param_part += "oauth_timestamp=" + url_params['oauth_timestamp']
+      param_part += "oauth_nonce=" + url_params['oauth_nonce']
 
-      url = @url_root + @url + '?'
+      to_sign += CGI::escape(param_part)
+
+      @logger.debug("Signing '#{to_sign}'")
+
+      sha1 = HMAC::SHA1.new(@signing_key)
+      sha1 << to_sign
+      signature = Base64.encode64(sha1.digest())
+
+      @logger.debug("signature == '#{signature}'")
+
+      url_params['oauth_signature'] = signature
+
+      url = @url + '?'
       url_params.keys.sort.each do |key|
         val = CGI::escape(url_params[key])
         url += "#{key}=#{val}&"
--- test/tc_signing.rb
+++ test/tc_signing.rb
@@ -7,21 +7,42 @@ include Gliffy
 class TC_testSigning < Test::Unit::TestCase
 
   def setup
-    @root = 'http://www.gliffy.com/rest'
-    @api_key = 'some_api_key'
-    @secret = 'some_big_secret'
+    @signed_url = SignedURL.new(:consumer_key => 'dpf43f3p2l4k3l03',
+                               :consumer_secret => 'kd94hf93k423kf44',
+                               :url => 'http://photos.example.net/photos',
+                               :access_token => 'nnch734d00sl2jdk',
+                               :access_secret => 'pfkkdhi9sl3r4s00',
+                               :method => 'GET')
   end
 
   def test_bad_param_override
-    url = '/accounts/FooBar/diagrams/45'
-    signed_url = SignedURL.new(@api_key,@secret,@root,url)
-    assert_raises(ArgumentError) do 
-      signed_url['apiKey'] = 'asdfasdfasdf'
+    SignedURL::READ_ONLY_PARAMS.keys.each do |param|
+      assert_raises(ArgumentError) do 
+        @signed_url[param] = 'asdfasdfasdf'
+      end
     end
   end
 
-  def test_sign_simple
+  def test_simple
+    @signed_url['file'] = 'vacation.jpg'
+    @signed_url['size'] = 'original'
 
+    expected_url = 'http://photos.example.net/photos?file=vacation.jpg&oauth_consumer_key=dpf43f3p2l4k3l03&oauth_nonce=kllo9940pd9333jh&oauth_signature=yLAzw2MLOraWc%2B6GbXchR8PzIJI%3D%0A&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1191242096&oauth_token=nnch734d00sl2jdk&oauth_version=1.0&size=original'
+    assert_equal(expected_url,@signed_url.full_url(1191242096,'kllo9940pd9333jh'))
+  end
+  def test_simple_bulk_params_set
+    params = {
+      'file' => 'vacation.jpg',
+      'size' => 'original',
+    }
+    @signed_url.params=params
+
+    expected_url = 'http://photos.example.net/photos?file=vacation.jpg&oauth_consumer_key=dpf43f3p2l4k3l03&oauth_nonce=kllo9940pd9333jh&oauth_signature=yLAzw2MLOraWc%2B6GbXchR8PzIJI%3D%0A&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1191242096&oauth_token=nnch734d00sl2jdk&oauth_version=1.0&size=original'
+    assert_equal(expected_url,@signed_url.full_url(1191242096,'kllo9940pd9333jh'))
+  end
+
+=begin
+  def test_sign_simple
     url = '/accounts/FooBar/diagrams/45'
     signed_url = SignedURL.new(@api_key,@secret,@root,url)
     assert_equal('http://www.gliffy.com/rest/accounts/FooBar/diagrams/45?apiKey=some_api_key&signature=347cb44d17a026351168e458f3691070',signed_url.full_url)
@@ -52,4 +73,5 @@ class TC_testSigning < Test::Unit::TestCase
   def assert_complex(signed_url)
     assert_equal('http://www.gliffy.com/rest/accounts/FooBar/diagrams/45?aaaa=crud&apiKey=some_api_key&blah=foo&signature=c117ddb1476984eb6c72af46b0a7548c&zzzz=booyah+booyah+booyah',signed_url.full_url)
   end
+=end
 end

fe4f7c1f4b45e4a541cec48c385412f5d841adeb
David Copeland <davetron5000@gmail.com>
2009-04-23 21:10:00 -0400

    removing unused code

--- lib/gliffy/commands.rb
+++
@@ -1,6 +0,0 @@
-require 'gliffy/commands/list'
-require 'gliffy/commands/delete'
-require 'gliffy/commands/url'
-require 'gliffy/commands/get'
-require 'gliffy/commands/edit'
-require 'gliffy/commands/new'

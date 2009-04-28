30478a481ef1c83e82d16131141ec7fb46aed893
David Copeland <davetron5000@gmail.com>
2009-04-28 07:59:10 -0400

    Removed lying comments.  Truth later

--- lib/gliffy/handle.rb
+++ lib/gliffy/handle.rb
@@ -10,18 +10,12 @@ module Gliffy
   # Since most calls to gliffy require a user-token, this class
   # encapsulates that token and the calls made under it.
   #
-  # The methods here are designed to raise exceptions if there are problems from Gliffy.
-  # These problems usually indicate a programming error or a server-side problem with Gliffy and
-  # are generally unhandleable.  However, if you wish to do something better than simply raise an exception
-  # you may override handle_error to do something else
-  #
   class Handle
 
     attr_reader :logger
     attr_reader :request
     attr_reader :token
 
-    # Create a new handle to gliffy for the given user.  Tokens will be requested as needed
     def initialize(api_root,credentials,http=nil,logger=nil)
       @credentials = credentials
       @request = Request.new(api_root,credentials)
@@ -84,7 +78,6 @@ module Gliffy
     # Get users in your account
     def account_users 
     end
-
 
     # Create a new diagram
     def diagram_create
